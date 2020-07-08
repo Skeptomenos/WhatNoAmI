5c4ca5427396d31c383680cd740e6c3805c5da7a
Skeptomenos <davidhelmus@gmail.com>
2020-07-08 22:03:41 +0200

    moved messages.py
    Signed-off-by: Skeptomenos <davidhelmus@gmail.com>

--- messages.py
+++
@@ -1,5 +0,0 @@
-welcome_message = "Hallo, hier ist WhatNoAmI das Zahenratespiel.\nDu bestimmst wie viele Zahlen es geben soll\nund ich generiere eine beliebiege Zahl dazwischen. Danach ratest du, welche Zahl ich gewählt habe.\n"
-give_highest_number = "Mit wie viel Zahlen möchtest du spielen?\n"
-correct = "Du hast richtig geraten!\n"
-lower = "Meine Zahl ist niedriger als deine.\n"
-higher = "Meine Zahl ist höher als deine.\n"
---
+++ src/messages.py
@@ -0,0 +1,9 @@
+def welcome_message():
+    print(
+        "Hallo, hier ist WhatNoAmI das Zahenratespiel.\nDu bestimmst wie viele Zahlen es geben soll\nund ich generiere eine beliebiege Zahl dazwischen. \nDanach ratest du, welche Zahl ich gewählt habe.\n")
+
+
+give_highest_number = "Mit wie viel Zahlen möchtest du spielen? "
+correct = "Du hast richtig geraten!\n"
+lower = "Meine Zahl ist niedriger als deine.\n"
+higher = "Meine Zahl ist höher als deine.\n"
